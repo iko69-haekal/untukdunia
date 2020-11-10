38e76f4582b8560f00fee63b820e1b90e6ecea2b
iko69 <ikobagas69@gmail.com>
2020-11-11 06:08:54 +0700

    sesuatu

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "jquery/src/jquery";
-import { hydrate, render } from "react-dom";
+import ReactDOM from "react-dom";
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
